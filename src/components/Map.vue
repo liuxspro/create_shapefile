6af75b3f719cd8224928fdb6ed21da0c1d34d69b
liuxsdev <liuxspro@gmail.com>
2024-02-07 21:08:26 +0800

    Chore: add north arrow

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,8 @@ import { ref, onMounted } from "vue";
 import { Map, View } from "ol";
 import XYZ from "ol/source/XYZ.js";
 import TileLayer from "ol/layer/Tile";
-import { defaults as defaultControls, ScaleLine } from "ol/control";
+import { defaults as defaultControls, ScaleLine, Zoom } from "ol/control";
+import Control from "ol/control/Control.js";
 
 import {
   NButton,
@@ -30,6 +31,32 @@ import {
   parse_csvdata,
 } from "./utils";
 
+// 指北针
+const northArrowControl = new Control({
+  element: createNorthArrowElement(),
+});
+
+northArrowControl.element.addEventListener("click", () => {
+  console.log(this);
+  olmap.getView().setRotation(0);
+});
+
+function createNorthArrowElement() {
+  var northArrowSvg =
+    '<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M32.5 23.6023V18.8588L18 59.1786L32.5 49.6916V48.0871L20.6412 55.6209L32.5 23.6023Z" fill="white"/><path d="M32.5 18.8588V49.6916L47 59.1786L32.5 18.8588Z" fill="white"/><path d="M38 4V16H35.8167L30.6153 8.44727H30.5277V16H28V4H30.2183L35.3789 11.5469H35.4839V4H38Z" fill="white"/></svg>';
+  var element = document.createElement("div");
+  element.innerHTML = northArrowSvg;
+  element.style.position = "absolute";
+  element.style.top = "15px"; // 调整 Y 坐标，以便控件位于右上角
+  element.style.right = "15px"; // 调整 X 坐标，以便控件位于右上角
+  return element;
+}
+
+// 旋转控件
+function rotateControl(rotation) {
+  var controlElement = northArrowControl.element;
+  controlElement.style.transform = "rotate(" + rotation + "rad)";
+}
 let olmap = ref();
 
 const necessary_fields_char = ["DKMC", "DKDM", "XZQMC", "XZQDM"];
@@ -62,6 +89,7 @@ const field_length = {
   BZ: 254,
 };
 let upload_ploygon;
+const map_rotate = ref(0); //地图旋转角度
 
 // 用于存储表单input的值
 const input_values = ref({});
@@ -85,7 +113,16 @@ function create_ol_map() {
       constrainResolution: true, // 将 resolution 约束为最接近的整数值
       projection: "EPSG:4326", // 4326 地图会变形
     }),
-    controls: defaultControls().extend([new ScaleLine({ units: "metric" })]),
+    controls: [new ScaleLine({ units: "metric" }), new Zoom()],
+  });
+  olmap.addControl(northArrowControl);
+  // 监听地图视图变化事件
+  olmap.getView().on("change:rotation", function (event) {
+    // 获取视图的旋转角度
+    map_rotate.value = event.target.getRotation();
+    console.log(map_rotate.value);
+    // 旋转控件
+    rotateControl(map_rotate.value);
   });
 }
 
