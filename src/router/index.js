41d23d4379ca594fe5c0c518649d28bb72d208db
liuxsdev <liuxspro@gmail.com>
2024-02-25 23:06:05 +0800

    Chore: 使用 dynamic import

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import IndexView from "../views/IndexView.vue";
-import CreateView from "../views/CreateView.vue";
+// import IndexView from "../views/IndexView.vue";
+// import CreateView from "../views/CreateView.vue";
 
 const routes = [
-  { path: "/", component: IndexView },
-  { path: "/create", component: CreateView },
+  { path: "/", component: () => import("../views/IndexView.vue") },
+  { path: "/create", component: () => import("../views/CreateView.vue") },
 ];
 
 const router = createRouter({
