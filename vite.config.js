3716b299360a1db4278c73946bed9206d63e7939
liuxsdev <liuxspro@gmail.com>
2024-03-05 22:58:03 +0800

    Chore: add commit info

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import replace from "@rollup/plugin-replace";
 import { VitePWA } from "vite-plugin-pwa";
+import { execSync } from "node:child_process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,6 +40,10 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+    }),
+    replace({
+      __COMMIT__: execSync("git rev-parse HEAD").toString().trim(),
+      __buildDate__: new Date().toLocaleString(),
     }),
   ],
   resolve: {
