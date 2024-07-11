8a1de3739b1ba183edeb49345db951e5e7202656
liuxspro <liuxspro@gmail.com>
2024-07-11 11:32:01 +0800

    Feat: 增加地块中心坐标显示

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from "vue";
 
 import { Map, View } from "ol";
-
+import { centerOfMass } from "@turf/center-of-mass";
 import { ScaleLine, Zoom } from "ol/control";
 import { baseMaps, create_text_style, create_polygon_style, NorthArrow } from "../utils/ol";
 import LayerSwitcher from "ol-layerswitcher";
@@ -62,10 +62,10 @@ const fileds_info = {
   BZ: "备注",
 };
 
-const upload_file_data = ref({ uploaded: false, file: {} });
+const upload_file_data = ref({ uploaded: false, file: {}, center: [0, 0] });
 
 let olmap;
-const map_rotate = ref(0); //地图旋转角度
+// const map_rotate = ref(0); //地图旋转角度
 const input_values = ref({ DKDM: "" }); // 保存表单 input (字段)的值
 const upload_points = ref({ lon_lat_points: [], proj_points: [], WKT: "", DH: 0 }); // 保存点位信息
 const vec_layer = ref();
@@ -140,11 +140,16 @@ async function handle_files() {
   upload_points.value = parse_coordinates_list(points);
   input_values.value["DH"] = upload_points.value.DH; // 自动填入带号
   const upload_ploygon = create_geojson_from_points(upload_points.value.lon_lat_points);
+  // const result = centroid(upload_ploygon);
+  const center = centerOfMass(upload_ploygon).geometry.coordinates
 
+  // console.log(result)
+  // console.log()
   vec_layer.value = create_vector_layer_from_geojson(upload_ploygon, false);
   olmap.addLayer(vec_layer.value);
   olmap.getView().fit(vec_layer.value.getSource().getExtent());
   upload_file_data.value.uploaded = true;
+  upload_file_data.value.center = center.map(i => i.toFixed(6)).toString()
 }
 
 function load_file() {
@@ -211,16 +216,15 @@ function create_shp() {
 
 <template>
   <div id="main" class="grow flex flex-col lg:flex-row h-full">
-    <div id="side" class="w-full lg:w-5/12 px-8 py-4 lg:py-8">
-      <div class="mb-2 p-1 lg:p-4 border border-slate-300 rounded-md">
+    <div id="side" class="w-full lg:w-5/12 px-8 py-4 lg:py-4">
+      <div class="mb-2 p-1 lg:p-2 border border-slate-300 rounded-md">
         <n-button quaternary @click="load_file" class="w-full text-left">
           <template #icon>
             <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 256 256">
               <g fill="currentColor">
                 <path d="M208 88h-56V32Z" opacity=".2" />
                 <path
-                  d="M48 180c0 11 7.18 20 16 20a14.24 14.24 0 0 0 10.22-4.66a8 8 0 0 1 11.56 11.06A30.06 30.06 0 0 1 64 216c-17.65 0-32-16.15-32-36s14.35-36 32-36a30.06 30.06 0 0 1 21.78 9.6a8 8 0 0 1-11.56 11.06A14.24 14.24 0 0 0 64 160c-8.82 0-16 9-16 20m79.6-8.69c-4-1.16-8.14-2.35-10.45-3.84c-1.25-.81-1.23-1-1.12-1.9a4.57 4.57 0 0 1 2-3.67c4.6-3.12 15.34-1.73 19.83-.56a8 8 0 0 0 4.14-15.48c-2.12-.55-21-5.22-32.84 2.76a20.58 20.58 0 0 0-9 14.95c-2 15.88 13.65 20.41 23 23.11c12.06 3.49 13.12 4.92 12.78 7.59c-.31 2.41-1.26 3.34-2.14 3.93c-4.6 3.06-15.17 1.56-19.55.36a8 8 0 0 0-4.31 15.44a61.34 61.34 0 0 0 15.19 2c5.82 0 12.3-1 17.49-4.46a20.82 20.82 0 0 0 9.19-15.23c2.19-17.31-14.32-22.14-24.21-25m83.09-26.84a8 8 0 0 0-10.23 4.84L188 184.21l-12.47-34.9a8 8 0 0 0-15.07 5.38l20 56a8 8 0 0 0 15.07 0l20-56a8 8 0 0 0-4.84-10.22M216 88v24a8 8 0 0 1-16 0V96h-48a8 8 0 0 1-8-8V40H56v72a8 8 0 0 1-16 0V40a16 16 0 0 1 16-16h96a8 8 0 0 1 5.66 2.34l56 56A8 8 0 0 1 216 88m-27.31-8L160 51.31V80Z"
-                />
+                  d="M48 180c0 11 7.18 20 16 20a14.24 14.24 0 0 0 10.22-4.66a8 8 0 0 1 11.56 11.06A30.06 30.06 0 0 1 64 216c-17.65 0-32-16.15-32-36s14.35-36 32-36a30.06 30.06 0 0 1 21.78 9.6a8 8 0 0 1-11.56 11.06A14.24 14.24 0 0 0 64 160c-8.82 0-16 9-16 20m79.6-8.69c-4-1.16-8.14-2.35-10.45-3.84c-1.25-.81-1.23-1-1.12-1.9a4.57 4.57 0 0 1 2-3.67c4.6-3.12 15.34-1.73 19.83-.56a8 8 0 0 0 4.14-15.48c-2.12-.55-21-5.22-32.84 2.76a20.58 20.58 0 0 0-9 14.95c-2 15.88 13.65 20.41 23 23.11c12.06 3.49 13.12 4.92 12.78 7.59c-.31 2.41-1.26 3.34-2.14 3.93c-4.6 3.06-15.17 1.56-19.55.36a8 8 0 0 0-4.31 15.44a61.34 61.34 0 0 0 15.19 2c5.82 0 12.3-1 17.49-4.46a20.82 20.82 0 0 0 9.19-15.23c2.19-17.31-14.32-22.14-24.21-25m83.09-26.84a8 8 0 0 0-10.23 4.84L188 184.21l-12.47-34.9a8 8 0 0 0-15.07 5.38l20 56a8 8 0 0 0 15.07 0l20-56a8 8 0 0 0-4.84-10.22M216 88v24a8 8 0 0 1-16 0V96h-48a8 8 0 0 1-8-8V40H56v72a8 8 0 0 1-16 0V40a16 16 0 0 1 16-16h96a8 8 0 0 1 5.66 2.34l56 56A8 8 0 0 1 216 88m-27.31-8L160 51.31V80Z" />
               </g>
             </svg>
           </template>
@@ -229,24 +233,30 @@ function create_shp() {
       </div>
       <div class="mb-2 p-4 lg:p-4 border border-slate-300 rounded-md" v-if="upload_file_data.uploaded">
         <div class="text-xs">
-          <span>当前文件: </span>
-          <code class="border border-slate-300 p-1 rounded-md">{{ upload_file_data.file.name }}</code>
+          <div>
+            <span>当前文件: </span>
+            <code class="border border-slate-300 p-1 rounded-md">{{ upload_file_data.file.name }}</code>
+          </div>
+          <div class="mt-3">
+            <span>中心点坐标: </span>
+            <code class="border border-slate-300 p-1 rounded-md">{{ upload_file_data.center }}</code>
+          </div>
+
+
         </div>
       </div>
       <!-- 填写字段 -->
       <div class="mb-2 p-4 lg:p-4 border border-slate-300 rounded-md">
         <n-collapse :default-expanded-names="['1']" class="min-h-4">
           <n-collapse-item name="1">
-            <template #header><h1 class="text-center w-full">填写字段</h1></template>
+            <template #header>
+              <h1 class="text-center w-full">填写字段</h1>
+            </template>
             <div class="mt-2">
               <n-form label-placement="left" :show-feedback="false">
                 <n-form-item label="调查阶段:" class="mb-3">
-                  <n-select
-                    v-model:value="select_stage"
-                    :options="stage_options"
-                    placeholder="选择调查阶段"
-                    @update:value="handleUpdateValue"
-                  />
+                  <n-select v-model:value="select_stage" :options="stage_options" placeholder="选择调查阶段"
+                    @update:value="handleUpdateValue" />
                 </n-form-item>
               </n-form>
             </div>
@@ -254,72 +264,35 @@ function create_shp() {
             <div id="fields">
               <!-- 必要字段 文本型 -->
               <n-form label-placement="left" label-width="5rem" show-require-mark :show-feedback="false">
-                <n-form-item
-                  :label="item"
-                  v-for="item in necessary_fields_char"
-                  :key="item"
-                  class="mb-3 dark:text-slate-100"
-                >
-                  <n-input
-                    size=""
-                    v-model:value="input_values[item]"
-                    :maxlength="FIELD_LENGTH[item]"
-                    show-count
-                    type="text"
-                    v-bind:placeholder="'请输入' + fileds_info[item]"
-                    @input="handleUpdateValue"
-                  />
+                <n-form-item :label="item" v-for="item in necessary_fields_char" :key="item"
+                  class="mb-3 dark:text-slate-100">
+                  <n-input size="" v-model:value="input_values[item]" :maxlength="FIELD_LENGTH[item]" show-count
+                    type="text" v-bind:placeholder="'请输入' + fileds_info[item]" @input="handleUpdateValue" />
                 </n-form-item>
               </n-form>
               <div>
                 <!-- 必要字段  地块面积和带号 -->
                 <n-form label-placement="left" show-require-mark :show-feedback="false">
                   <n-form-item label="YDMJ" class="mb-3">
-                    <n-input-number
-                      size="medium"
-                      :precision="2"
-                      v-model:value="input_values['YDMJ']"
-                      v-bind:placeholder="'请输入' + fileds_info['YDMJ']"
-                      clearable
-                      class="w-full"
-                      label-width="5rem"
-                    />
+                    <n-input-number size="medium" :precision="2" v-model:value="input_values['YDMJ']"
+                      v-bind:placeholder="'请输入' + fileds_info['YDMJ']" clearable class="w-full" label-width="5rem" />
                   </n-form-item>
                   <n-form-item label="DH" class="mb-3">
-                    <n-input-number
-                      size="medium"
-                      :precision="0"
-                      v-model:value="input_values['DH']"
-                      v-bind:placeholder="'请输入' + fileds_info['DH']"
-                      clearable
-                      class="w-full"
-                      label-width="5rem"
-                      show-require-mark
-                    />
+                    <n-input-number size="medium" :precision="0" v-model:value="input_values['DH']"
+                      v-bind:placeholder="'请输入' + fileds_info['DH']" clearable class="w-full" label-width="5rem"
+                      show-require-mark />
                   </n-form-item>
                 </n-form>
               </div>
               <!-- 日期 -->
               <n-form label-placement="left" :show-feedback="false">
                 <n-form-item label="SCRQ" class="mb-3">
-                  <n-date-picker
-                    v-model:value="input_values['SCRQ']"
-                    type="date"
-                    value-format="yyyy-MM-dd"
-                    placeholder="请输入日期"
-                    class="w-full"
-                  />
+                  <n-date-picker v-model:value="input_values['SCRQ']" type="date" value-format="yyyy-MM-dd"
+                    placeholder="请输入日期" class="w-full" />
                 </n-form-item>
                 <n-form-item :label="item" v-for="item in other_fields" :key="item" class="mb-3">
-                  <n-input
-                    size="medium"
-                    v-model:value="input_values[item]"
-                    :maxlength="FIELD_LENGTH[item]"
-                    show-count
-                    type="text"
-                    v-bind:placeholder="'请输入' + fileds_info[item]"
-                    class="w-full"
-                  />
+                  <n-input size="medium" v-model:value="input_values[item]" :maxlength="FIELD_LENGTH[item]" show-count
+                    type="text" v-bind:placeholder="'请输入' + fileds_info[item]" class="w-full" />
                 </n-form-item>
               </n-form>
               <!-- 其他字段 -->
@@ -330,17 +303,15 @@ function create_shp() {
       <div class="mb-2 p-1 lg:p-4 border border-slate-300 rounded-md">
         <n-button quaternary type="success" class="w-full" @click="create_shp" :disabled="!upload_file_data.uploaded">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 256 256">
-            <path
-              fill="currentColor"
-              d="M184 144h-16a8 8 0 0 0-8 8v56a8 8 0 0 0 16 0v-8h8a28 28 0 0 0 0-56m0 40h-8v-24h8a12 12 0 0 1 0 24m-48-32v56a8 8 0 0 1-16 0v-56a8 8 0 0 1 16 0m-40 56a8 8 0 0 1-8 8H56a8 8 0 0 1-7-12l25.16-44H56a8 8 0 0 1 0-16h32a8 8 0 0 1 7 12l-25.21 44H88a8 8 0 0 1 8 8M213.66 82.34l-56-56A8 8 0 0 0 152 24H56a16 16 0 0 0-16 16v72a8 8 0 0 0 16 0V40h88v48a8 8 0 0 0 8 8h48v16a8 8 0 0 0 16 0V88a8 8 0 0 0-2.34-5.66M160 80V51.31L188.69 80Z"
-            />
+            <path fill="currentColor"
+              d="M184 144h-16a8 8 0 0 0-8 8v56a8 8 0 0 0 16 0v-8h8a28 28 0 0 0 0-56m0 40h-8v-24h8a12 12 0 0 1 0 24m-48-32v56a8 8 0 0 1-16 0v-56a8 8 0 0 1 16 0m-40 56a8 8 0 0 1-8 8H56a8 8 0 0 1-7-12l25.16-44H56a8 8 0 0 1 0-16h32a8 8 0 0 1 7 12l-25.21 44H88a8 8 0 0 1 8 8M213.66 82.34l-56-56A8 8 0 0 0 152 24H56a16 16 0 0 0-16 16v72a8 8 0 0 0 16 0V40h88v48a8 8 0 0 0 8 8h48v16a8 8 0 0 0 16 0V88a8 8 0 0 0-2.34-5.66M160 80V51.31L188.69 80Z" />
           </svg>
           &nbsp;生成边界文件
         </n-button>
       </div>
     </div>
 
-    <div id="map" class="px-8 py-4 lg:py-8 lg:pl-0 lg:pr-8 w-full lg:h-auto lg:min-h-[800px] h-[400px]"></div>
+    <div id="map" class="px-8 py-4 lg:py-4 lg:pl-0 lg:pr-8 w-full lg:h-auto lg:min-h-[800px] h-[400px]"></div>
   </div>
 </template>
 
