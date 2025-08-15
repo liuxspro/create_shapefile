e33fb8521a5ebe0ecdc73c8b28b4c535abb875d9
liuxspro <liuxspro@gmail.com>
2025-08-16 00:07:14 +0800

    Refactor: 使用 create_dbf_field_value 函数简化字段值生成逻辑

--- src/utils/dbfwrite.js
+++ src/utils/dbfwrite.js
@@ -1,3 +1,4 @@
+import { create_dbf_field_value } from "@liuxspro/utils";
 // 字段长度
 const FIELD_LENGTH = {
   DKMC: 254,
@@ -11,6 +12,7 @@ const FIELD_LENGTH = {
   BZ: 254,
 };
 // 预定义好的文件头信息 占用321字节
+// deno-fmt-ignore
 const header_data = new Uint8Array([
   0x03, 0x7c, 0x02, 0x03, 0x01, 0x00, 0x00, 0x00, 0x41, 0x01, 0xf8, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x44, 0x4b, 0x4d, 0x43, 0x00, 0x00,
@@ -35,47 +37,20 @@ const header_data = new Uint8Array([
 const RECORD_FLAG = new Uint8Array([0x20]);
 const EOF = new Uint8Array([0x1a]);
 
-function create_data_array(string, length) {
-  // 使用 TextEncoder 将字符串转为 Uint8Array
-  const encoder = new TextEncoder();
-  const stringUint8Array = encoder.encode(string);
-  // 创建一个长度为 <length> 的 Uint8Array
-  const uint8Array = new Uint8Array(length);
-  // 将转换后的 Uint8Array 复制到新 Uint8Array 的前面部分
-  uint8Array.set(stringUint8Array, 0);
-  // 后面剩余的部分填充16进制数值20
-  uint8Array.fill(0x20, stringUint8Array.length);
-  return uint8Array;
-}
-
-function create_data_array_right(string, length) {
-  // 使用 TextEncoder 将字符串转为 Uint8Array
-  const encoder = new TextEncoder();
-  const stringUint8Array = encoder.encode(string);
-  // 创建一个长度为 <length> 的 Uint8Array
-  const uint8Array = new Uint8Array(length);
-  //   填充16进制数值20
-  uint8Array.fill(0x20);
-  // 计算需要填充的空间大小
-  const paddingSize = Math.max(0, length - stringUint8Array.length);
-  // 将转换后的 Uint8Array 复制到新 Uint8Array 的最后面部分
-  uint8Array.set(stringUint8Array, paddingSize);
-  return uint8Array;
-}
-
 function generate_fields_array(fields) {
   /**
    * 根据字段信息，生成 Uint8Array
    */
   const arrays = [];
+  // deno-fmt-ignore
   const fields_order = ["DKMC", "DKDM", "XZQDM", "XZQMC", "YDMJ", "DH", "SCRQ", "SCDW", "BZ"]; // 按此字段顺序写入
   for (let key of fields_order) {
     if (fields.hasOwnProperty(key) && FIELD_LENGTH[key]) {
       let array;
       if (key === "YDMJ" || key == "DH") {
-        array = create_data_array_right(fields[key], FIELD_LENGTH[key]);
+        array = create_dbf_field_value(fields[key], FIELD_LENGTH[key], "right");
       } else {
-        array = create_data_array(fields[key], FIELD_LENGTH[key]);
+        array = create_dbf_field_value(fields[key], FIELD_LENGTH[key]);
       }
       arrays.push(array);
     }
@@ -87,7 +62,6 @@ function concatenate(resultConstructor, ...arrays) {
   /**
    * 合并所有的 Uint8Array
    * https://www.cnblogs.com/qinmengjiao123-123/p/7194169.html
-   *
    */
   let totalLength = 0;
   for (let arr of arrays) {
@@ -105,7 +79,12 @@ function concatenate(resultConstructor, ...arrays) {
 function create_dbf(fields) {
   const array_list = generate_fields_array(fields);
   const arrays = concatenate(Uint8Array, ...array_list);
-  const total = concatenate(Uint8Array, ...[header_data, RECORD_FLAG, arrays, EOF]);
+  const total = concatenate(Uint8Array, ...[
+    header_data,
+    RECORD_FLAG,
+    arrays,
+    EOF,
+  ]);
   return total;
 }
 
