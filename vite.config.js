6c9632c2160b61e1bf401c78b3057182db202714
liuxspro <liuxspro@gmail.com>
2025-10-13 14:06:44 +0800

    Refactor: 将 VitePWA 配置提取为单独常量

--- vite.config.js
+++ vite.config.js
@@ -6,56 +6,58 @@ import { VitePWA } from "vite-plugin-pwa";
 import { execSync } from "node:child_process";
 import { version } from "./package.json";
 
+const pwa = VitePWA({
+  includeAssets: ["favicon.ico", "favicon.svg"],
+  registerType: "autoUpdate",
+  injectRegister: "auto",
+  workbox: {
+    globPatterns: ["assets/*", "**/*.{js,css,html}"],
+  },
+  manifest: {
+    name: "制作地块边界文件",
+    short_name: "制作地块边界文件",
+    description: "制作地块边界文件",
+    theme_color: "#E2E8F0",
+    start_url: "/",
+    display: "standalone",
+    id: "liuxspro.createshapefile",
+    icons: [
+      {
+        src: "pwa-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    screenshots: [{
+      src: "screenshot.png",
+      sizes: "1200x600",
+      type: "image/png",
+      form_factor: "wide",
+    }, {
+      src: "screenshot.png",
+      sizes: "1200x600",
+      type: "image/png",
+    }],
+  },
+  devOptions: {
+    enabled: true,
+  },
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      includeAssets: ["favicon.ico", "favicon.svg"],
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      workbox: {
-        globPatterns: ["assets/*", "**/*.{js,css,html}"],
-      },
-      manifest: {
-        name: "制作地块边界文件",
-        short_name: "制作地块边界文件",
-        description: "制作地块边界文件",
-        theme_color: "#E2E8F0",
-        start_url: "/",
-        display: "standalone",
-        id: "liuxspro.createshapefile",
-        icons: [
-          {
-            src: "pwa-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        screenshots: [{
-          src: "screenshot.png",
-          sizes: "1200x600",
-          type: "image/png",
-          form_factor: "wide",
-        }, {
-          src: "screenshot.png",
-          sizes: "1200x600",
-          type: "image/png",
-        }],
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
     replace({
       __COMMIT__: execSync("git rev-parse HEAD").toString().trim(),
       __buildDate__: new Date().toLocaleString(),
       __version__: version,
+      preventAssignment: true,
     }),
   ],
   resolve: {
