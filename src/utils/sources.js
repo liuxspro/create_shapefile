9bbe11d4ed14839cbd0fa5f23fd6ade13a350216
liuxspro <liuxspro@gmail.com>
2026-01-14 12:42:22 +0800

    Fix: 文件类型检查csv

--- src/utils/sources.js
+++ src/utils/sources.js
@@ -10,7 +10,7 @@ export async function parse_upload_files(file_list) {
   const result = [];
 
   for (const [_index, file] of Array.from(file_list).entries()) {
-    if (file.type === "text/csv") {
+    if (file.type === "text/csv" || file.name.toLowerCase().endsWith(".csv")) {
       const csv_result = await parse_csv_file(file);
       result.push(csv_result);
     }
